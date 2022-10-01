7b740f281377fa01204a2d8e03b8ff677ba9c486
Robert Grochowski <robert.grochowski@a4bee.com>
2022-10-01 23:00:49 +0200

    new algorithm implementation

--- backend/get-notion/app.js
+++ backend/get-notion/app.js
@@ -1,15 +1,28 @@
 const AWS = require('aws-sdk');
-const uuid = require('uuid');
 const dynamodb = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
 
-exports.lambdaHandler = function(event, context, callback) {
-    const dynamoQueryParams = {
-        TableName : "NOTION",
-        Limit: 1,
-        ExclusiveStartKey: {
-            'uuid': uuid.v1()
+exports.lambdaHandler = function (event, context, callback) {
+    const getQueryParams = endDate => {
+        return {
+            TableName: "NOTION",
+            ExpressionAttributeValues: {
+                ':fromDate': endDate.getTime()
+            },
+            FilterExpression: 'addedDate >= :fromDate'
         }
     };
+
+    const executeQuery = (endDate) =>
+        new Promise((resolve, reject) => {
+            dynamodb.scan(getQueryParams(endDate),
+                (err, data) => err ? reject(err) : resolve(data.Items))
+        });
+
+    const getDateWithWeeksAgo = weeks => {
+        let date = new Date()
+        date.setDate(date.getDate() - weeks * 7);
+        return date;
+    }
 
     const response = {
         statusCode: 200,
@@ -21,14 +34,63 @@ exports.lambdaHandler = function(event, context, callback) {
         body: "",
     }
 
-    dynamodb.scan(dynamoQueryParams, function(err, data) {
-        if (err) {
-            response.statusCode = 500
-            response.body = 'DynamoDB Scan Error: ' + err;
+    const today = new Date();
+    const onSuccess = (items) => {
+        let result = []
+        let lastWeekItems = []
+        let lastThreeWeeksItems = []
+
+
+        items.forEach(notion => {
+            let date = new Date(notion.addedDate);
+            let daysDifference = Math.ceil((today - date) / (1000 * 3600 * 24));
+            console.log(daysDifference)
+            if (daysDifference <= 7) {
+                lastWeekItems.push(notion);
+            } else {
+                lastThreeWeeksItems.push(notion);
+            }
+        });
+
+        lastThreeWeeksItems = shuffle(lastThreeWeeksItems);
+        lastThreeWeeksItems = lastThreeWeeksItems.slice(0, 30)
+        for(let i = 0; i < 3; i++) {
+            result.push(lastWeekItems)
         }
-        else {
-            response.body = JSON.stringify(data.Items[0]);
-        }
+        result.push(lastThreeWeeksItems);
+        result = shuffle(result)
+
+        response.body = JSON.stringify(result);
+        callback(null, response)
+    }
+
+    const onError = (error) => {
+        console.error(error);
+        response.statusCode = 500
+        response.body = 'DynamoDB Query Error: ' + err;
         callback(null, response);
-    });
+    }
+
+    executeQuery(getDateWithWeeksAgo(4))
+        .then(onSuccess)
+        .catch(onError)
+
+
+    function shuffle(array) {
+        let currentIndex = array.length, randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex !== 0) {
+
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
 };
